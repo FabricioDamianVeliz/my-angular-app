869ed36423ca77afd58f8808aa0e174ff2d6ab61
FabricioDamianVeliz <fabricio.damian.veliz@gmail.com>
2023-07-13 23:00:55 -0300

    Cambios
    Modifiqué los archivos app.component.css, app.component.html, app.component.ts y app.module.ts en src/app
    Modifiqué el archivo index.html en src

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,78 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
-interface Item {
+export interface ProductData {
+  id: string;
   name: string;
   cost: string;
 }
 
+const NAMES: string[] = [
+  'Washing Machine', 'Refrigerator', 'Television', 'Blender', 'Oven', 'Microwave', 'Vacuum Cleaner',
+  'Coffee Maker', 'Mixer', 'Iron', 'Dryer', 'Fan', 'Toaster', 'Juicer', 'Kettle',
+  'Fryer', 'Kitchen Robot', 'Air Conditioner', 'Stove', 'Teapot', 'Sandwich Maker', 'Steamer',
+  'Water Dispenser', 'Water Heater', 'Alarm Clock', 'Electric Brush', 'Air Purifier'
+];
+
+const getRandomName = (): string => {
+  const randomIndex = Math.floor(Math.random() * NAMES.length);
+  return NAMES[randomIndex];
+};
+
+const PRODUCTS: ProductData[] = Array.from({ length: 100 }, (_, index) => ({
+  id: (index + 1).toString(),
+  name: getRandomName(),
+  cost: `U$S ${Math.floor(Math.random() * 100) + 1}`
+}));
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  items: Item[] = [
-    { name: 'Producto 1', cost: '$10' },
-    { name: 'Producto 2', cost: '$20' },
-    { name: 'Producto 3', cost: '$30' }
-  ];
+export class AppComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'cost'];
+  dataSource: MatTableDataSource<ProductData>;
 
-  showList = true;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  toggleList() {
-    this.showList = !this.showList;
+  constructor() {
+    this.dataSource = new MatTableDataSource(PRODUCTS);
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  showTable = false;
+
+  toggleTable() {
+    this.showTable = !this.showTable;
+    if (this.showTable) {
+      setTimeout(() => {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      });
+    }
+  }
+  
 }
+
+
+
 
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +21,13 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
